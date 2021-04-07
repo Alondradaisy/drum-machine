b5ff301ec1eaaa7af4816c0a741d7079ebc2a771
Alondra Warren <daisy.codeimmersives.com>
2021-04-06 20:23:51 -0700

    set counter

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Setup 'tick' sound
-const tick = new Audio('sounds/tick.mp3');
+const tick = new Audio('sounds/tick.mp3'); 
 
 // This function is called every 600ms
 function update() {
@@ -10,11 +10,31 @@ function update() {
 
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
-    setInterval(update, 600);
+    setInterval(update, 600); //ticks
+    setInterval(updateTock, 2400); //6*4 =24
+    setInterval(count, 600) //counts
 }
 
 // Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
 
+const tock = new Audio('sounds/tock.mp3'); // plays tock audio 
+
+function updateTock() {
+    tock.play();
+} 
+
+let counter = 0;
+
 const checkbox = document.querySelector("#metronome").checked = true;
 console.log(checkbox.checked);
+
+const counting = document.querySelector('.counter');
+
+function count () {
+    counting.innerHTML = counter;
+    if(counter < 4) {
+        counter++;
+    } else (counter === 1)
+    
+}
